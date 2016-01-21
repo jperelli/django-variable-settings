ff82121441fed8f6311143e2d55a4d669053014d
Julian Perelli <jperelli@gmail.com>
2016-01-21 12:07:18 -0300

    management commands testing

--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+#from django.test import TestCase
+from unittest import TestCase  # use this to keep data in db
 
-# Create your tests here.
+from django.core.management import call_command
+
+class ManagementTest(TestCase):
+
+    def test_variable_settings_initialize(self):
+        call_command('variable_settings_initialize')
