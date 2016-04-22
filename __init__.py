eeff1c4f590e07704ce8cd460b1984d96b02a2b3
Maxi Padulo <maxi.padulo@gmail.com>
2016-04-22 14:30:10 +0200

    refactor and cleaning

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
-
 def get(key):
     from .models import Setting
     import json
     # key can be "alerting.ficon.*" or without asterisk
     # value can be a plain string or a json object
-    
+
     if len(key) == 0:
         raise Exception("An empty key was received")
-    
+
     if key[-1] == "*":
         d = {}
         for s in Setting.objects.filter(key__startswith = key[:-1]):
             d.update({s.key: json.loads(s.value)})
         return d
-    
+
     # let it raise an exception if the key is not found
     return json.loads(Setting.objects.get(key = key).value)
 
